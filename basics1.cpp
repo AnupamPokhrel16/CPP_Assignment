e69e6cbfb9157427c9fb489d3add2f2fe36e66fe
Anupam Pokhrel <147306707+AnupamPokhrel16@users.noreply.github.com>
2025-02-06 20:59:56 +0545

    initial comit

---
+++ basics1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+
+string first, second;
+cout<< "enter first name";
+cin>> first;
+cout <<"enter second name";
+cin >> second;
+cout << "the reverse name is "<<second<<" "<<first;
+return 0;
+}
